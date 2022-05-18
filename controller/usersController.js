827134695e8a54c56b3f5c5c63c6fe47c247548a
shagor7 <40910111+shagor7@users.noreply.github.com>
2022-05-18 12:48:55 +0600

    ghpala hoisilo
    dekha jak thik hoise ki na

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // external imports
 const bcrypt = require("bcrypt");
 const { unlink } = require("fs");
@@ -83,50 +82,10 @@ async function removeUser(req, res, next) {
             },
         });
     }
-=======
-//externam imports
-const bcrypt = require("bcrypt");
-
-//get Users page
-function getUsers(req, res, next) {
-    res.render("users");
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-}
-
-//add user
-async function addUser(req, res, next) {
-    let newUser;
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    if (req.files && req.files.length > 0) {
-        newUser = new User({
-            ...req.body,
-            avatar: req.files[0].filename,
-            password: hashedPassword,
-        });
-    }
-
-    //save user or send error
-    try {
-        const result = await newUser.save();
-        res.status(200).json({
-            message: "user was added successfully",
-        });
-    } catch (err) {
-        res.status(500).json({
-            error: {
-                common: {
-                    msg: "Unknown error occured!",
-                },
-            },
-        });
-    }
 }
 
 module.exports = {
     getUsers,
     addUser,
-<<<<<<< HEAD
     removeUser,
-=======
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
 };
--- middlewares/users/userValidators.js
+++ middlewares/users/userValidators.js
@@ -1,135 +1,38 @@
-<<<<<<< HEAD
 // external imports
-=======
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-const { check, validationResult } = require("express-validator");
-const createError = require("http-errors");
-const path = require("path");
-const { unlink } = require("fs");
+const express = require("express");
+const { check } = require("express-validator");
 
-<<<<<<< HEAD
 // internal imports
-const User = require("../../models/People");
-
-// add user
-const addUserValidators = [
-    check("name")
-        .isLength({ min: 1 })
-        .withMessage("Name is required")
-        .isAlpha("en-US", { ignore: " -" })
-        .withMessage("Name must not contain anything other than alphabet")
-        .trim(),
-    check("email")
-        .isEmail()
-        .withMessage("Invalid email address")
-        .trim()
-        .custom(async (value) => {
-            try {
-                const user = await User.findOne({ email: value });
-                if (user) {
-                    throw createError("Email already is use!");
-=======
-//add user
-const adduserValidators = [
-    check("name")
-        .isLength({ min: 1 })
-        .withMessage("name is required")
-        .isAlpha("en-US", { ignore: " -" })
-        .withMessage(" Name must not contain anything other than alphabet")
-        .trim(),
-    check("email")
-        .isEmail()
-        .withMessage("invalid email address")
-        .trim()
-        .custom(async (value) => {
-            try {
-                const user = await user.findOne({ email: value });
-                if (user) {
-                    throw createError("Email already in use!");
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-                }
-            } catch (err) {
-                throw createError(err.message);
-            }
-        }),
-    check("mobile")
-        .isMobilePhone("bn-BD", {
-            strictMode: true,
-        })
-        .withMessage("Mobile number must be a valid Bangladeshi mobile number")
-        .custom(async (value) => {
-            try {
-<<<<<<< HEAD
-                const user = await User.findOne({ mobile: value });
-                if (user) {
-                    throw createError("Mobile already is use!");
-=======
-                const user = await user.findOne({ mobile: value });
-                if (user) {
-                    throw createError(" mobile number already in use!");
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-                }
-            } catch (err) {
-                throw createError(err.message);
-            }
-        }),
-    check("password")
-        .isStrongPassword()
-        .withMessage(
-<<<<<<< HEAD
-            "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
-        ),
-];
-
-const addUserValidationHandler = function (req, res, next) {
-=======
-            "Password must be atleast 8 charecter's long $ should contain atleast 1 lowercase, 1 uppercase, 1 number and 1 symbol"
-        ),
-];
-
-const adduserValidationHandler = function (req, res, next) {
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-    const errors = validationResult(req);
-    const mappedErrors = errors.mapped();
-    if (Object.keys(mappedErrors).length === 0) {
-        next();
-    } else {
-<<<<<<< HEAD
-        // remove uploaded files
-        if (req.files.length > 0) {
-            const { filename } = req.files[0];
-            unlink(
-                path.join(__dirname, `/../public/uploads/avatars/${filename}`),
-=======
-        //remove uploader files
-        if (req.files.length > 0) {
-            const { filename } = req.files[0];
-            unlink(
-                path.join(__dirname, `/../public/uploads/avatar/${filename}`),
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-                (err) => {
-                    if (err) console.log(err);
-                }
-            );
-        }
-<<<<<<< HEAD
-
-        // response the errors
-=======
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
-        res.status(500).json({
-            errors: mappedErrors,
-        });
-    }
-};
-
-module.exports = {
-<<<<<<< HEAD
+const {
+    getUsers,
+    addUser,
+    removeUser,
+} = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
     addUserValidators,
     addUserValidationHandler,
-};
-=======
-    adduserValidators,
-    adduserValidationHandler,
-}
->>>>>>> d7d1bbb8a11cecb9fa831c1a61d1289e3b2590bc
+} = require("../middlewares/users/userValidators");
+
+const { checkLogin } = require("../middlewares/common/checkLogin");
+
+const router = express.Router();
+
+// users page
+router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
+
+// add user
+router.post(
+    "/",
+    checkLogin,
+    avatarUpload,
+    addUserValidators,
+    addUserValidationHandler,
+    addUser
+);
+
+// remove user
+router.delete("/:id", removeUser);
+
+module.exports = router;
